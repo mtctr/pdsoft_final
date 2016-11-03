79cfe3543c882c62dc417ea321d09d6796c8f9c2
Igor Macedo <igormacedo94@yahoo.com.br>
2016-11-02 21:44:11 -0300

    New inserts to populate-data

--- populate-data.sql
+++ populate-data.sql
@@ -1,1 +1,10 @@
 insert into admin values ("admin", "admin");
+
+insert into validadores(num_serie)
+            values("a1b2c3");
+
+insert into validadores(num_serie)
+            values("a1b2c4");
+
+insert into validadores(num_serie)
+            values("a1b2c5");
